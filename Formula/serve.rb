70c9749d7fd961165392b5cb97546d3bd0321725
Chase Hutchins <syntaqx@gmail.com>
2019-01-14 19:40:49 -0700

    fix whitespace

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -3,7 +3,7 @@ class Nfpm < Formula
     homepage "https://github.com/syntaqx/serve"
     url "https://github.com/syntaqx/serve/releases/download/v0.0.1/serve_0.0.1_macos_x86_64.tar.gz"
     version "0.0.1"
-    sha256 "b478e5968fcd26ef6cf8e8f7a750490ffe07337d9304460f13c8485d497b59c3  "
+    sha256 "b478e5968fcd26ef6cf8e8f7a750490ffe07337d9304460f13c8485d497b59c3"
 
     def install
       bin.install "serve"
