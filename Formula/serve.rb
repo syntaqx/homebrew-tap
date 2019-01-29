6e81bdf6d00849c226dea379f3ac2c6536c0e086
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-29 11:21:17 -0700

    Brew formula update for serve version v0.3.0

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -1,9 +1,9 @@
 class Serve < Formula
   desc "serve is a static http server anywhere you need one."
   homepage "https://github.com/syntaqx/serve"
-  url "https://github.com/syntaqx/serve/releases/download/v0.2.0/serve_0.2.0_macos_x86_64.tar.gz"
-  version "0.2.0"
-  sha256 "95951376c655c512578de440a2f455cfb1b3a8fc20dc34ac6e9474eba87846ba"
+  url "https://github.com/syntaqx/serve/releases/download/v0.3.0/serve_0.3.0_macos_x86_64.tar.gz"
+  version "0.3.0"
+  sha256 "7f132af742033e3bf7b6c227a5f6a95f199ad292737634f4b2a75bce1e911f8d"
 
   def install
     bin.install "serve"
