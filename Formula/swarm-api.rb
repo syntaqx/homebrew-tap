9483804abcad42aaa88ff63a2bcafbec32b1f865
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-25 20:27:14 -0600

    Brew formula update for swarm-api version v0.0.4

--- Formula/swarm-api.rb
+++ Formula/swarm-api.rb
@@ -2,9 +2,9 @@
 class SwarmApi < Formula
   desc "swarm-api is a dead simple REST API for Docker Swarm"
   homepage "https://github.com/syntaqx/swarm-api"
-  url "https://github.com/syntaqx/swarm-api/releases/download/v0.0.3/swarm-api_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "880933cedad95c5ca61616064c0873bd4c80929425bf00c38a4fc84646cd0930"
+  url "https://github.com/syntaqx/swarm-api/releases/download/v0.0.4/swarm-api_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "9fc3997a97148fee5b0c79cbcc6fbc9e837641ec6952bc6a1d468c584a64cacf"
 
   def install
     bin.install "swarm-api"
